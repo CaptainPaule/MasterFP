4cc865cc0f5838c5b7c867657fff99aa834b426f
Paul Becker <paul.becker@tu-dortmund.de>
2017-11-20 17:14:53 +0100

    Auswertung bis i)

--- optischesPumpen/scripts/plotLande.py
+++ optischesPumpen/scripts/plotLande.py
@@ -122,6 +122,10 @@ g1 = (4 * math.pi)/(A1 * 10**(-9)) * m/e
 g2 = (4 * math.pi)/(A2 * 10**(-9)) * m/e
 B = (B1 + B2) / 2
 
+# note:
+# g1 is Rb87
+# g2 is Rb85
+
 # export results as latexEquation
 exportResultAsEquation("g_{F1}", g1, 3, 3, "", "resG1")
 exportResultAsEquation("g_{F2}", g2, 3, 3, "", "resG2")
@@ -138,23 +142,21 @@ exportResultAsEquation("h_2", amp2, 1, 1, "\\text{px}", "ratioRes2")
 exportResultAsEquation("r", ratio, 3, 3, "", "ratio")
 
 # calculate core spin
-# F = 1 ground state
 # and J = 1/2 (Alkali)
 # define electron g factor
 ge = ufloat(2.00231930436182, 0.00000000000052)
 
 # the core spin is a discret value
-I = np.linspace(-5, 5, 21)
+I = np.linspace(0, 5, 11)
 
 # define quantum numbers for selected transition
-F = 1
+F = 2
 J = 1/2
-
 
 # calc the ratio of the electron g factor and the spectific isotop g factor
 # and build a numpy array
-lhs1 = np.full(21, (g1/ge).n)
-lhs2 = np.full(21, (g2/ge).n)
+lhs1 = np.full(11, (g1/ge).n)
+lhs2 = np.full(11, (g2/ge).n)
 
 # calculate the secound side of equation and look for grahical solution
 #define function
@@ -169,13 +171,13 @@ def rhs(I, F, J):
 plt.plot(I, rhs(I, F, J), 'bo', label="xxx")
 plt.plot(I, lhs1, 'rx', label="ddd")
 
-plt.axvline(x = 0.5, linewidth=2, color = 'k')
+plt.axvline(x = (1.5), linewidth=2, color = 'k')
 plt.xlabel('I')
 plt.ylabel(r'$\frac{g_F}{g_J}$')
 
-#plt.xticks([0, -0.5, -1, -1.5, -2, -2.5, -3, -3.5, -4, -4.5, -5],
-#           [r"$-0$", r"$-\frac{1}{2}$", r"$-1$", r"$-\frac{3}{2}$", r"$-2$", r"$-\frac{5}{2}$",
-#		    r"$-3$", r"$-\frac{7}{2}$", r"$-4$", r"$-\frac{9}{2}$", r"$-5$"])
+plt.xticks([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5],
+           [r"$0$", r"$\frac{1}{2}$", r"$1$", r"$\frac{3}{2}$", r"$2$", r"$\frac{5}{2}$",
+		    r"$3$", r"$\frac{7}{2}$", r"$4$", r"$\frac{9}{2}$", r"$5$"])
 
 # create grid and legend
 plt.grid()
@@ -188,16 +190,17 @@ plt.savefig('../img/coreSpin1.pdf')
 plt.clf()
 
 # plot Isotop 2 coreSpin
+F = 3
 plt.plot(I, rhs(I, F, J), 'bo', label="xxx")
 plt.plot(I, lhs2, 'rx', label="ddd")
 
-plt.axvline(x = 0.5, linewidth=2, color = 'k')
+plt.axvline(x = 2.5, linewidth=2, color = 'k')
 plt.xlabel('I')
 plt.ylabel(r'$\frac{g_F}{g_J}$')
 
-#plt.xticks([0, -0.5, -1, -1.5, -2, -2.5, -3, -3.5, -4, -4.5, -5],
-#           [r"$-0$", r"$-\frac{1}{2}$", r"$-1$", r"$-\frac{3}{2}$", r"$-2$", r"$-\frac{5}{2}$",
-#		    r"$-3$", r"$-\frac{7}{2}$", r"$-4$", r"$-\frac{9}{2}$", r"$-5$"])
+plt.xticks([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5],
+           [r"$0$", r"$\frac{1}{2}$", r"$1$", r"$\frac{3}{2}$", r"$2$", r"$\frac{5}{2}$",
+		    r"$3$", r"$\frac{7}{2}$", r"$4$", r"$\frac{9}{2}$", r"$5$"])
 
 # create grid and legend
 plt.grid()
@@ -206,8 +209,19 @@ plt.legend(loc='best')
 # save file
 plt.savefig('../img/coreSpin2.pdf')
 
-#clear plot
+# clear plot
 plt.clf()
+
+# quadratischer Zeeman Effekt
+Ehyp = 10**(-24)
+B = sweepSpule(2.50) + horizontalSpule(0.25)
+BohrMag = 10**(-24)
+
+UHqz = (g1.n)**2 * BohrMag**2 * B**2 / Ehyp
+
+print("Abgeschätzte Größe des quadratischen Zeeman Effektes")
+print(UHqz)
+print("")
 
 # read data
 A1, P1, deltaT1 = np.genfromtxt('../data/messwertePeriodenResonanz1.txt', unpack=True)
@@ -218,47 +232,47 @@ def hyp(x, a, b, c):
 	return a + b * np.power(x-c, -1)
 
 # calc and plot fit isotop 1
-params, cov = curve_fit(hyp, P1, A1)
-errors = np.sqrt(np.diag(cov))
+#params, cov = curve_fit(hyp, P1, A1)
+#errors = np.sqrt(np.diag(cov))
 
-print("Fit Amplitude/Periode Isotop 1")
-print('a =', params[0], '+/-', errors[0])
-print('b =', params[1], '+/-', errors[1])
-print("")
+#print("Fit Amplitude/Periode Isotop 1")
+#print('a =', params[0], '+/-', errors[0])
+#print('b =', params[1], '+/-', errors[1])
+#print("")
 
-x = np.linspace(0, 15, 50)
-plt.plot(P1, A1, 'bo')
-plt.plot(x, hyp(x, *params), 'r-')
+#x = np.linspace(0, 15, 50)
+#plt.plot(P1, A1, 'bo')
+#plt.plot(x, hyp(x, *params), 'r-')
 
 # create grid and legend
-plt.grid()
-plt.legend(loc='best')
+#plt.grid()
+#plt.legend(loc='best')
 
 # save file
-plt.savefig('../img/trans1.pdf')
+#plt.savefig('../img/trans1.pdf')
 
 #clear plot
-plt.clf()
+#plt.clf()
 
 # calc and plot fit isotop 2
-params, cov = curve_fit(hyp, P2, A2)
-errors = np.sqrt(np.diag(cov))
+#params, cov = curve_fit(hyp, P2, A2)
+#errors = np.sqrt(np.diag(cov))
 
-print("Fit Amplitude/Periode Isotop 2")
-print('a =', params[0], '+/-', errors[0])
-print('b =', params[1], '+/-', errors[1])
-print("")
+#print("Fit Amplitude/Periode Isotop 2")
+#print('a =', params[0], '+/-', errors[0])
+#print('b =', params[1], '+/-', errors[1])
+#print("")
 
-x = np.linspace(0, 15, 500)
-plt.plot(P2, A2, 'bo')
-plt.plot(x, hyp(x, *params), 'r-')
+#x = np.linspace(0, 15, 500)
+#plt.plot(P2, A2, 'bo')
+#plt.plot(x, hyp(x, *params), 'r-')
 
 # create grid and legend
-plt.grid()
-plt.legend(loc='best')
+#plt.grid()
+#plt.legend(loc='best')
 
 # save file
-plt.savefig('../img/trans1.pdf')
+#plt.savefig('../img/trans1.pdf')
 
 #clear plot
-plt.clf()
+#plt.clf()
