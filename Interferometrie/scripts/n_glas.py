d524435e93bd0a138fd3156041ec157b4948fafc
LuTse <alina.esfahani@tu-dortmund.de>
2017-11-30 14:14:08 +0100

    include error propagation

--- Interferometrie/scripts/n_glas.py
+++ Interferometrie/scripts/n_glas.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 theta, M_1, M_2, M_3 = np.genfromtxt('../data/n_glas.txt', unpack=True)
 M = np.mean([M_1, M_2, M_3], axis=0)
-print(M)
+M_error = np.std([M_1, M_2, M_3], axis=0)
 
 def n_glas(x, a, b):
     return a * x + b
 
-params, covariance_matrix = curve_fit(n_glas, theta, M)
+params, covariance_matrix = curve_fit(n_glas, theta, M, sigma=M_error)
 errors = np.sqrt(np.diag(covariance_matrix))
 
 print('a =', params[0], '±', errors[0])
 print('b =', params[1], '±', errors[1])
 
-plt.plot(theta, M, '+')
+plt.errorbar(theta, M, yerr=M_error, fmt='+')
 plt.plot(theta, n_glas(theta, *params))
 plt.show()
