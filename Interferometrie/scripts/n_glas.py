8a4fe2c0f8b38cfc7f9aabf0da2e75b356630d56
LuTse <alina.esfahani@tu-dortmund.de>
2017-12-20 20:38:26 +0100

    fix plots and evaluation of index of refraction

--- Interferometrie/scripts/n_glas.py
+++ Interferometrie/scripts/n_glas.py
@@ -2,22 +2,36 @@ from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Eigenschaften Glas
+T = 0.001
+lamba = 633*10**(-9)
+alpha = 160*(20*np.pi)/360
+
 theta, M_1, M_2, M_3 = np.genfromtxt('../data/n_glas.txt', unpack=True)
 M = np.mean([M_1, M_2, M_3], axis=0)
 M_error = np.std([M_1, M_2, M_3], axis=0)
 
-def n_glas(x, a, b):
-    return a * x + b
+nL = 1.00029499
+#Theoriekurve M(teta)
+def n_glas(x, a):
+    ta = (10 + x)/180*np.pi
+    ta2 = (10 - x)/180*np.pi
+    ti = np.arcsin(nL/a*np.sin(ta))
+    ti2 = np.arcsin(nL/a*np.sin(ta2))
+    return T/lamba * ( (a - np.cos(ta - ti))/np.cos(ti)*nL - (a - np.cos(ta2 - ti2))/np.cos(ti2)*nL )
 
 params, covariance_matrix = curve_fit(n_glas, theta, M, sigma=M_error)
 errors = np.sqrt(np.diag(covariance_matrix))
 
 print('a =', params[0], '±', errors[0])
-print('b =', params[1], '±', errors[1])
 
 plt.errorbar(theta, M, yerr=M_error, fmt='+', label='gemittelte Messwerte')
-plt.plot(theta, n_glas(theta, *params), label='Fit')
+plt.plot(theta, n_glas(theta, *params), label='Fit an gemittelte Werte')
+plt.plot(theta, M_1, 'x', label='Messwerte 1.Messung')
+plt.plot(theta, M_2, 'x', label='Messwerte 2.Messung')
+plt.plot(theta, M_3, 'x', label='Messwerte 3.Messung')
 plt.xlabel(r'$\theta/°$')
-plt.ylabel(r'#Intensitätsmaxima')
+plt.ylabel(r'$M(\theta)$')
 plt.legend()
+plt.tight_layout()
 plt.savefig('../img/n_glas.png')
--- Interferometrie/scripts/n_luft.py
+++ Interferometrie/scripts/n_luft.py
@@ -1,27 +1,47 @@
 from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
 
-theta, M_1, M_2, M_3 = np.genfromtxt('../data/n_luft.txt', unpack=True)
+# Länge von Gas in Meter
+L = 0.1
+# Wellenlänge vom Licht
+lamb = 633e-9
+
+druck, M_1, M_2, M_3 = np.genfromtxt('../data/n_luft.txt', unpack=True)
+M_1 = M_1 * lamb / L + 1
+M_2 = M_2 * lamb / L + 1
+M_3 = M_3 * lamb / L + 1
 M = np.mean([M_1[:16], M_2[:16], M_3[:16]], axis=0)
 M_error = np.std([M_1[:16], M_2[:16], M_3[:16]], axis=0)
 M = np.append(M, np.mean([M_1[16:], M_2[16:]], axis=0))
 M_error = np.append(M_error, np.std([M_1[16:], M_2[16:]], axis=0))
-M_error[M_error == 0] = 1.
-print(M_error)
+M_error[M_error == 0.] = 1e-8
+print(M_1, M_error)
 
-def n_glas(x, a, b):
-    return a * x + b
+def n_glas(x, a):
+    return np.sqrt(a * x + 1)
 
-params, covariance_matrix = curve_fit(n_glas, theta, M, p0=[0.05, -1.3], sigma=M_error)
+params, covariance_matrix = curve_fit(n_glas, druck, M, sigma=M_error)
 errors = np.sqrt(np.diag(covariance_matrix))
 
-print('a =', params[0], '±', errors[0])
-print('b =', params[1], '±', errors[1])
+print('a =', params, '±', errors)
 
-plt.errorbar(theta, M, yerr=M_error, fmt='+', label='gemittelte Messwerte')
-plt.plot(theta, n_glas(theta, *params), label='Fit')
+F = ufloat(params, errors)
+T0 = ufloat(15, 0)
+T = ufloat(22.2, 0)
+F_norm = F * T / T0
+n_norm = unp.sqrt(1 - F_norm * 1013)
+print('Brechungsindex bei Normalbedingungen: ', n_norm)
+
+plt.errorbar(druck, M, yerr=M_error, fmt='+', label='gemittelte Messwerte')
+plt.plot(druck, n_glas(druck, *params), label='Fit an gemittelte Werte')
+plt.plot(druck, M_1, 'x', label='Messwerte 1.Messung')
+plt.plot(druck, M_2, 'x', label='Messwerte 2.Messung')
+plt.plot(druck, M_3, 'x', label='Messwerte 3.Messung')
 plt.xlabel(r'Druck/mbar')
-plt.ylabel(r'#Intensitätsmaxima')
+plt.ylabel(r'$n_{Luft}$')
 plt.legend(loc='upper left')
+plt.tight_layout()
 plt.savefig('../img/n_luft.png')
