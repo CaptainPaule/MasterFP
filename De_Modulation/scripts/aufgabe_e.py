1aed0431f8b02da0715a0e9fcd2d245b62bf1bde
LuTse <alina.esfahani@tu-dortmund.de>
2018-03-02 18:14:23 +0100

    start evaluation on part e)

---
+++ De_Modulation/scripts/aufgabe_e.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+omega_T, U, U_0 = np.genfromtxt('../data/aufgabe_e.txt', unpack=True)
+
+plt.plot(omega_T, U, 'C0')
+plt.plot(omega_T, U_0, 'C1')
+plt.show()
