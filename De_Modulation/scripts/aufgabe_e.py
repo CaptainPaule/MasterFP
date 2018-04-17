8593910e0f6869cb75755bd8d68a4f5f65818656
LuTse <alina.esfahani@tu-dortmund.de>
2018-04-17 17:59:47 +0200

    hoffentlich alles außer Theorieteil fertig

--- De_Modulation/scripts/aufgabe_e.py
+++ De_Modulation/scripts/aufgabe_e.py
@@ -3,23 +3,27 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+# rescale
+delta_T = 250e-9
+
 def fit(x, A, B):
 	return A*x + B
 
 # import data
 omega_T, U, U_0 = np.genfromtxt('../data/aufgabe_e.txt', unpack=True)
 
+omega_T = omega_T * 1e6
 # linear fit
-params, cov = curve_fit(fit, omega_T, np.arccos(U/U_0))
+params, cov = curve_fit(fit, omega_T * delta_T, np.arccos(U/U_0))
 errors = np.sqrt(np.diag(cov))
 
 print("Fit Function")
 print('A =', params[0], '+/-', errors[0])
 print('B =', params[1], '+/-', errors[1])
 
-plt.plot(omega_T, np.arccos(U/U_0), 'kx', label="data")
-plt.plot(omega_T, fit(omega_T, *params), 'r-', label="fit")
-plt.xlabel(r'$\omega_T$')
+plt.plot(omega_T*delta_T, np.arccos(U/U_0), 'kx', label="data")
+plt.plot(omega_T*delta_T, fit(omega_T*delta_T, *params), 'r-', label="fit")
+plt.xlabel(r'$\varphi$')
 plt.ylabel(r'$\arccos(U/U_0)$')
 plt.legend()
 plt.show()
