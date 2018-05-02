76ec5ac9055ecbc203f5e71ecce136787150b862
LuTse <alina.esfahani@tu-dortmund.de>
2018-05-02 17:37:16 +0200

    2. Korrektur

--- De_Modulation/scripts/aufgabe_e.py
+++ De_Modulation/scripts/aufgabe_e.py
@@ -6,8 +6,11 @@ from scipy.optimize import curve_fit
 # rescale
 delta_T = 250e-9
 
-def fit(x, A, B):
-	return A*x + B
+#def fit(x, A, B):
+#	return A*x + B
+
+def fit(x, B):
+        return -x + B
 
 # import data
 omega_T, U, U_0 = np.genfromtxt('../data/aufgabe_e.txt', unpack=True)
@@ -18,8 +21,8 @@ params, cov = curve_fit(fit, omega_T * delta_T, np.arccos(U/U_0))
 errors = np.sqrt(np.diag(cov))
 
 print("Fit Function")
-print('A =', params[0], '+/-', errors[0])
-print('B =', params[1], '+/-', errors[1])
+#print('A =', params[0], '+/-', errors[0])
+print('B =', params[0], '+/-', errors[0])
 
 plt.plot(omega_T*delta_T, np.arccos(U/U_0), 'kx', label="data")
 plt.plot(omega_T*delta_T, fit(omega_T*delta_T, *params), 'r-', label="fit")
