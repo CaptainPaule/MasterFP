fb765b4e379e7a679aaaf41fff180ba4d3ddf4a3
LuTse <alina.esfahani@tu-dortmund.de>
2018-06-03 11:39:01 +0200

    finish TAC calibration

--- KosmMyonen/scripts/calib.py
+++ KosmMyonen/scripts/calib.py
@@ -5,21 +5,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-compressed_data = []
+channel_number = []
 data = np.genfromtxt('../data/Kalibrierung_TAC.txt', unpack=True)
 
-x = 0
-y = 0
+# get the relevant data from calibration measurement
+y = []
+for id in range(len(data)):
+    if data[id]!=0:
+        y.append(id)
+    if (data[id]==0 and y):
+        channel_number.append(round(np.mean(y)))
+        y = []
 
-for i, val in zip(range(len(data)), data):
-    if val!=0:
-        y = y + val
-        x = 0
-    if (val==0 and x==0):
-        compressed_data.append(y)
-        x = x + 1
-        y = 0
+# define delay times in ns
+delay = [0.2, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.4, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 9.9]
 
-channel = range(len(compressed_data))
-plt.plot(channel[1:], compressed_data[1:], 'rx')
+def fit(x, A):
+    return A*x
+
+# linear fit
+params, cov = curve_fit(fit, channel_number, delay)
+errors = np.sqrt(np.diag(cov))
+
+print("Fit Function")
+print('A =', params[0], '+/-', errors[0])
+
+plt.plot(channel_number, fit(channel_number, params), 'g-', label="fit")
+plt.plot(channel_number, delay, 'rx', label='data')
+plt.legend()
+plt.xlabel('Kanalnummer')
+plt.ylabel('Laufzeit in ns')
 plt.savefig('../img/calib.png')
