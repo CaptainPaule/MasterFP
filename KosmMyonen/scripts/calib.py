c5122c580ae525169c32c33459d5746ecdfb7f7b
Paul Becker <paul.becker@tu-dortmund.de>
2018-06-05 14:56:38 +0200

    set axis limits and add secound fit parameter

--- KosmMyonen/scripts/calib.py
+++ KosmMyonen/scripts/calib.py
@@ -20,19 +20,32 @@ for id in range(len(data)):
 # define delay times in ns
 delay = [0.2, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.4, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 9.9]
 
-def fit(x, A):
-    return A*x
+# convert to np array
+channel_number = np.asarray(channel_number)
+delay = np.asarray(delay)
+
+def fit(x, A, B):
+    return A * x + B
 
 # linear fit
 params, cov = curve_fit(fit, channel_number, delay)
 errors = np.sqrt(np.diag(cov))
 
+# print out fit results
 print("Fit Function")
 print('A =', params[0], '+/-', errors[0])
+print('A =', params[1], '+/-', errors[1])
 
-plt.plot(channel_number, fit(channel_number, params), 'g-', label="fit")
+# plot
+plt.plot(channel_number, fit(channel_number, *params), 'g-', label="fit")
 plt.plot(channel_number, delay, 'rx', label='data')
+
+# set axis limits
+plt.xlim(5, 219)
+plt.ylim(0.2, 9.9)
+
+# save fig
 plt.legend()
 plt.xlabel('Kanalnummer')
-plt.ylabel('Laufzeit in ns')
+plt.ylabel('Laufzeit \ ns')
 plt.savefig('../img/calib.png')
