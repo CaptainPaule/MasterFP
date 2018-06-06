eafa668ffba577314a320d90c00d686d8a91f6f4
Paul Becker <paul.becker@tu-dortmund.de>
2018-06-06 16:01:34 +0200

    evaluation

--- KosmMyonen/scripts/kosmyon.py
+++ KosmMyonen/scripts/kosmyon.py
@@ -6,29 +6,33 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def unbinned_array(binned_array, start=0, end=None,
-                   calibration_func=lambda x: x):
-    """ Returned an array with 'raw' Data for creating histograms with plt.hist
-    """
-    raw_data = []
-    if end is None:
-        end = len(binned_array)
-    for x in range(start, end):
-        raw_data.extend([calibration_func(x)] * binned_array[x])
-    return np.array(raw_data)
+def build_bins(start, end, num):
+	return np.linspace(start, end, num)
+
 
 data = np.genfromtxt('../data/Messung.txt', unpack=True)
-unbinned_data = unbinned_array(data)
-bins = 50
-y, binEdges = np.histogram(unbinned_data, bins=bins)
-bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
-width = 0.5 * (bincenters[-1] - bincenters[0]) / bins
-medStd = np.sqrt(y)
-plt.errorbar(bincenters, y, fmt='ro', yerr=medStd, xerr=width)
+bins = 256.0
+start = 1.0
+end = 512.0
 
-x_data = np.arange(0, len(data))
-decay_function = lambda x, a, b: a * np.exp(b * x)
-coeff, var = curve_fit(decay_function, x_data, data, p0=[1, -1e-2])
-plt.plot(x_data, decay_function(x_data, coeff[0], coeff[1]))
-plt.xlim(0, 400)
+# print histogram
+hist, edges = np.histogram(data, bins=build_bins(start, end, bins))
+
+print(hist)
+
+# evaluate bin centers
+bin_centers = 0.5 * (edges[1:] + edges[:-1])
+
+# calc width
+bin_width = 0.5 * (bin_centers[-1] - bin_centers[0]) / bins
+
+print("bin width: %s" % bin_width)
+print("centers: ")
+
+# plot bin centers
+plt.bar(edges[:-1], hist, width=end/bins)
+plt.plot(bin_centers, hist, 'rx')
+
+
+plt.xlim(1, 512)
 plt.show()
